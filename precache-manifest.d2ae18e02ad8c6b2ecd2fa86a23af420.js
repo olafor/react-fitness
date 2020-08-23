dacd1ccc190dd62a502b7fef866e41b921785caa
Ola Fornander <ofornander@gmail.com>
2020-08-23 10:33:08 +0200

    Updates

---
+++ precache-manifest.d2ae18e02ad8c6b2ecd2fa86a23af420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cb450f3362ca49989edc0ee51e3289",
+    "url": "/react-fitness/index.html"
+  },
+  {
+    "revision": "8c955342fdd7c1ae3bd7",
+    "url": "/react-fitness/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5748137ac98a52dc04e8",
+    "url": "/react-fitness/static/js/2.b0322c10.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/react-fitness/static/js/2.b0322c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c955342fdd7c1ae3bd7",
+    "url": "/react-fitness/static/js/main.82825c1a.chunk.js"
+  },
+  {
+    "revision": "e178e27e5ac9e1f0889e",
+    "url": "/react-fitness/static/js/runtime-main.15ce7a12.js"
+  }
+]);
